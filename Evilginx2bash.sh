784edf6e1fc53d57fc3c90457df2f9e4de1de58c
Jay <19259608+jayhill365@users.noreply.github.com>
2022-09-24 15:04:23 -0700

    Update Evilginx2bash.sh

--- Evilginx2bash.sh
+++ Evilginx2bash.sh
@@ -4,5 +4,6 @@ sudo apt-get install make
 git clone https://github.com/jayhill365/evilginx2.git
 cd evilginx2
 sudo make install
+sudo apt-get install apache2
 sudo apache2 service stop 
 sudo ./evilginx
