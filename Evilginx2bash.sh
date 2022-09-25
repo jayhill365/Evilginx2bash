46e7e36dabc3c03b69a3e9baa01425123e44e0b3
Jay <19259608+jayhill365@users.noreply.github.com>
2022-09-25 03:52:16 -0700

    Update Evilginx2bash.sh

--- Evilginx2bash.sh
+++ Evilginx2bash.sh
@@ -1,6 +1,15 @@
 sudo apt-get update
 sudo apt-get install git-all
 sudo apt-get install make
+mkdir -p ~/git/GitHub/canha
+cd ~/git/GitHub/canha
+git clone https://github.com/canha/golang-tools-install-script
+cd golang-tools-install-script/
+bash goinstall.sh --64
+cd ..
+cd ..
+cd ..
+cd ..
 git clone https://github.com/jayhill365/evilginx2.git
 cd evilginx2
 sudo make install
